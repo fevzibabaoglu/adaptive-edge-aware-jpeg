ed16ada4c7dcdcea01db19501617fd5a8cd6caa2
Fevzi Babaoğlu <fevzibabaoglu03@gmail.com>
2025-03-11 00:24:02 +0300

    feat: implement decompress method

--- src/jpeg/jpeg.py
+++ src/jpeg/jpeg.py
@@ -19,11 +19,13 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import cv2 as cv
 import numpy as np
+from collections import deque
 
 from color import apply_normalization, convert
 from image import Image
 from .edge_detection import EdgeDetection
 from .quadtree import QuadTree
+from .utils import largest_power_of_2
 
 
 class Jpeg:
@@ -95,7 +97,32 @@ class Jpeg:
         chrom_2_quantized = Jpeg.quantize(chrom_2_dct, self.quality)
 
         #TODO to be continued
-        pass
+
+    def decompress(self, lum_quantized, chrom_1_quantized, chrom_2_quantized):
+        #TODO to be implemented
+
+        lum_dct = Jpeg.dequantize(lum_quantized, self.quality)
+        chrom_1_dct = Jpeg.dequantize(chrom_1_quantized, self.quality)
+        chrom_2_dct = Jpeg.dequantize(chrom_2_quantized, self.quality)
+
+        lum_blocks = Jpeg.inverse_dct(lum_dct)
+        chrom_1_blocks = Jpeg.inverse_dct(chrom_1_dct)
+        chrom_2_blocks = Jpeg.inverse_dct(chrom_2_dct)
+
+        lum_downsampled = Jpeg.block_merge(lum_blocks, self.settings['color_space'], self.settings['layer_shapes']['lum'])
+        chrom_1_downsampled = Jpeg.block_merge(chrom_1_blocks, self.settings['color_space'], self.settings['layer_shapes']['chrom_1'])
+        chrom_2_downsampled = Jpeg.block_merge(chrom_2_blocks, self.settings['color_space'], self.settings['layer_shapes']['chrom_2'])
+
+        lum = Jpeg.upsample(lum_downsampled, self.layer_shape)
+        chrom_1 = Jpeg.upsample(chrom_1_downsampled, self.layer_shape)
+        chrom_2 = Jpeg.upsample(chrom_2_downsampled, self.layer_shape)
+
+        img_color_converted = Image.from_array(np.stack([lum, chrom_1, chrom_2], axis=2))
+
+        img = Jpeg.color_conversion_inverse(img_color_converted.get_flattened(), self.settings['color_space'])
+        img = Image.from_array(img, img_color_converted.original_shape)
+
+        return img
 
     @staticmethod
     def color_conversion(flattened_img, color_space):
@@ -138,6 +165,46 @@ class Jpeg:
 
             blocks.append(block)
         return blocks
+
+    @staticmethod
+    def block_merge(blocks, color_space, shape):
+        # Initialize the matrix with zeros
+        H, W = shape
+        node_size = largest_power_of_2(max(H, W)) * 2
+        image_layer = np.zeros((node_size, node_size), dtype=np.float32)
+        leaves_deque = deque(blocks)
+
+        def rebuild_layer(x, y, node_size):
+            if not leaves_deque:
+                return
+            if x >= W or y >= H:
+                return
+            if node_size == 0:
+                return
+
+            current_leaf = leaves_deque[0]
+            leaf_size = current_leaf.shape[0]
+
+            # Check if the current region matches the leaf
+            if node_size == leaf_size:
+                image_layer[y:y+node_size, x:x+node_size] = current_leaf
+                leaves_deque.popleft()
+
+            # Split into quadrants
+            else:
+                child_node_size = node_size // 2
+                rebuild_layer(x, y, child_node_size)                                      # Top-left
+                rebuild_layer(x + child_node_size, y, child_node_size)                    # Top-right
+                rebuild_layer(x, y + child_node_size, child_node_size)                    # Bottom-left
+                rebuild_layer(x + child_node_size, y + child_node_size, child_node_size)  # Bottom-right
+
+        rebuild_layer(0, 0, node_size)
+        image_layer = image_layer[:H, :W]
+        reshaped_image_layer = np.empty((image_layer.size, 3), dtype=image_layer.dtype)
+        reshaped_image_layer[:, 0] = image_layer.flatten()
+        denormalized_image = apply_normalization(color_space, reshaped_image_layer, True)
+        denormalized_image = denormalized_image[:, 0].reshape(image_layer.shape)
+        return denormalized_image
 
     @staticmethod
     def dct(blocks):
